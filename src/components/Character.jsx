5a7ac528f49d2f688dbebf250f1e9ddae9ae7630
ColmanNicolas <busimaximiliano@gmail.com>
2024-06-30 01:51:36 -0300

    fix-cards

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,17 +1,13 @@
-import { useParams } from "react-router-dom"
 
 function Character({character}) {
 
- const parametros = useParams()
- console.log(parametros.id)
   return (
-      <div className="text-center p-5">
-        <h3>{character.name}</h3>
-        <img className="img-fluid rounded-pill" src={character.image} alt={character.name} />
-        <p className="m-2 fw-semibold">{character.origin.name}</p>
-        <button onClick={()=>{
-          console.log(character)
-        }} className="btn btn-dark">Info..</button>
+<div className="card text-bg-dark text-center m-3 border-0">
+        <img src={character.image} className="card-img-top rounded-pill" alt={character.name}/>
+        <div className="card-body">
+          <h5 className="card-title">{character.name}</h5>
+          <p className="card-text">{character.origin.name}</p>   
+        </div>
       </div>
   )
 }
