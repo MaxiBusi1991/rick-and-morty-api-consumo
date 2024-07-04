16b132d494be18eaae4f102445be1587560723d7
ColmanNicolas <busimaximiliano@gmail.com>
2024-07-04 16:52:12 -0300

    se agrega title y favicon

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rick-and-morty-api-consumo/',
+  base: 'https://MaxiBusi1991.github.io/rick-and-morty-api-consumo/',
   plugins: [react()],
 })
